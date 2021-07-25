da6027122f10e394eec5c8db50435eee4eebba26
Bunyad Ahmadli <bunyad.ahmadli@gmail.com>
2021-07-26 00:53:34 +0300

    added logout

--- controllers/authController.go
+++ controllers/authController.go
@@ -11,18 +11,19 @@ import (
 )
 
 const SecretKey = "secret"
+
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
 
-	if err:=c.BodyParser(&data); err!=nil {
+	if err := c.BodyParser(&data); err != nil {
 		return err
 	}
-	password, _ :=bcrypt.GenerateFromPassword([]byte(data["password"]),14)
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
 
-	user:= models.User{
-		Name: data["name"],
+	user := models.User{
+		Name:     data["name"],
 		Password: password,
-		Email: data["email"],
+		Email:    data["email"],
 		Username: data["username"],
 	}
 	database.Db.Create(&user)
@@ -33,56 +34,68 @@ func Register(c *fiber.Ctx) error {
 func Login(c *fiber.Ctx) error {
 	var data map[string]string
 
-	if err:=c.BodyParser(&data); err!=nil {
+	if err := c.BodyParser(&data); err != nil {
 		return err
 	}
 
 	var user models.User
 
-	database.Db.Where("email = ?",data["email"]).First(&user)
-	if user.ID==0 {
+	database.Db.Where("email = ?", data["email"]).First(&user)
+	if user.ID == 0 {
 		c.Status(fiber.StatusNotFound)
-		return c.JSON(fiber.Map{"message":"user not found"})
+		return c.JSON(fiber.Map{"message": "user not found"})
 	}
-	if err:=bcrypt.CompareHashAndPassword(user.Password,[]byte(data["password"])); err!=nil{
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
 		c.Status(fiber.StatusBadRequest)
-		return c.JSON(fiber.Map{"message":"incorrect password"})
+		return c.JSON(fiber.Map{"message": "incorrect password"})
 	}
-	claims := jwt.NewWithClaims(jwt.SigningMethodHS256,jwt.StandardClaims{
-		Issuer: strconv.Itoa(int(user.ID)),
-		ExpiresAt: time.Now().Add(time.Hour*24).Unix(),
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.ID)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
 	})
 
-	token,err := claims.SignedString([]byte(SecretKey))
-	if err!=nil {
+	token, err := claims.SignedString([]byte(SecretKey))
+	if err != nil {
 		c.Status(fiber.StatusInternalServerError)
-		return c.JSON(fiber.Map{"message":"could not log in"})
+		return c.JSON(fiber.Map{"message": "could not log in"})
 	}
-	cookie :=fiber.Cookie{
-		Name: "jwt",
-		Value: token,
-		Expires: time.Now().Add(time.Hour*24),
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
 		HTTPOnly: true,
 	}
 	c.Cookie(&cookie)
 	return c.JSON(fiber.Map{
-		"message":"success",
+		"message": "success",
 	})
 }
 func User(c *fiber.Ctx) error {
-	cookie:=c.Cookies("jwt")
+	cookie := c.Cookies("jwt")
 
-	token,err := jwt.ParseWithClaims(cookie,&jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte(SecretKey),nil
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
 	})
 
-	if err!=nil {
+	if err != nil {
 		c.Status(fiber.StatusUnauthorized)
-		return c.JSON(fiber.Map{"message":"unauthenticated"})
+		return c.JSON(fiber.Map{"message": "unauthenticated"})
 	}
-	claims:=token.Claims.(*jwt.StandardClaims)
+	claims := token.Claims.(*jwt.StandardClaims)
 
 	var user models.User
-	database.Db.Where("id = ?",claims.Issuer).First(&user)
+	database.Db.Where("id = ?", claims.Issuer).First(&user)
 	return c.JSON(user)
+}
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -5,10 +5,11 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func Setup(app *fiber.App)  {
+func Setup(app *fiber.App) {
 	api := app.Group("/api")
-	api.Post("/register",controllers.Register)
-	api.Post("/login",controllers.Login)
-	api.Get("/user",controllers.User)
+	api.Post("/register", controllers.Register)
+	api.Post("/login", controllers.Login)
+	api.Get("/user", controllers.User)
+	api.Post("/logout", controllers.Logout)
 
 }
